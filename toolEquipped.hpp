4e5677ad34e0bacf4763d26edb68836c88813328
5721006 <123737243+5721006@users.noreply.github.com>
2023-07-08 08:20:04 +0800

    更新 toolEquipped.hpp

--- toolEquipped.hpp
+++ toolEquipped.hpp
@@ -3,46 +3,103 @@
 #include "src/Chef.hpp"
 #include "src/Values.hpp"
 
-// 关于技法：
-// 例：skill->ability.add(30);     // 全技法+30
-// 例：skill->ability.bake += 150; // 烤技法+150,
-//   bake 烤；knife 切；stirfry 炒；fry 炸；steam 蒸；boil 煮
-
-// 关于技法售价：
-// 例：skill->abilityBuff.bake += 25; // 烤售价+25%
-
-// 关于口味售价：
-// 例：skill->flavorBuff.sweet += 30; // 甜售价+30%
-//   sweeet 甜；spicy 辣；salty 咸；tasty 鲜；sour 酸；bitter 苦
-
-// 关于食材售价：
-// 例：skill->materialBuff.vegetable += 15; // 蔬菜售价+15%
-//   vegetable 菜类；meat 肉类；fish 鱼类；creation 面类
-
-// 关于金币获得：
-// 例：skill->coinBuff += 30; // 金币获得+30%
-
 void toolEquipped(Chef *chef) {
+
+    auto skill = &chef->skill;
+
+    if (chef->name == "凯丽") {    // 鎏金之锋
+        skill->ability.knife += 150; // 切技法+150
+        chef->tool = NO_TOOL;          // 不允许装备其他厨具
+    }
+
+    if (chef->name == "宋书汐汐") {    // 凤尾扫把
+        skill->materialBuff.vegetable += 20; // 蔬菜售价+20%
+        chef->tool = NO_TOOL;          // 不允许装备其他厨具
+    }
+   
+    if (chef->name == "姒露") {    // 金风扇
+        skill->materialBuff.meat += 20; // 肉售价+20%
+        chef->tool = NO_TOOL;          // 不允许装备其他厨具
+    }
+
+    if (chef->name == "藻白") {    // 金刚磨刀钢
+        skill->abilityBuff.knife += 20; // 切类售价+20%
+        chef->tool = NO_TOOL;          // 不允许装备其他厨具
+    }  
+
+    if (chef->name == "息夫人") {    // 金玉勺
+        skill->abilityBuff.boil += 20; // 煮售价+20%
+        chef->tool = NO_TOOL;          // 不允许装备其他厨具
+    }
+    
+    if (chef->name == "黑柏") {    // 银制剪刀
+        skill->ability.knife += 80; // 切技法+80
+        skill->materialBuff.fish += 10; // 水产售价+10%
+        chef->tool = NO_TOOL;          // 不允许装备其他厨具
+    }
+
+    if (chef->name == "龙波儿") {    // 银制水壶
+        skill->ability.boil += 80; // 煮技法+80
+        skill->materialBuff.fish += 10; // 水产售价+10%
+        chef->tool = NO_TOOL;          // 不允许装备其他厨具
+    }
+    
+    if (chef->name == "府先生") {    // 精致面包机
+        skill->ability.bake += 80; // 烤技法+80
+        skill->materialBuff.creation += 10; // 面类售价+10%
+        chef->tool = NO_TOOL;          // 不允许装备其他厨具
+    }
+
+    if (chef->name == "樊欣") {    // 贵族私家托盘
+        skill->ability.bake += 90; // 烤技法+90
+        skill->abilityBuff.knife += 10; // 煮售价+10%
+        chef->tool = NO_TOOL;          // 不允许装备其他厨具
+    }
+
+    if (chef->name == "郑国民父女") {    // 金印大碗
+        skill->abilityBuff.fry += 20; // 炸售价+20%
+        chef->tool = NO_TOOL;          // 不允许装备其他厨具
+    }
+    
+    if (chef->name == "兰染") {    // 金猫筷子架
+        skill->ability.fry += 150; // 炸技法+150
+        chef->tool = NO_TOOL;          // 不允许装备其他厨具
+    }
+
+    if (chef->name == "尖椒鸡") {    // 反重力酒提
+        skill->ability.bake += 150; // 烤技法+150
+        chef->tool = NO_TOOL;          // 不允许装备其他厨具
+    }
+
+    if (chef->name == "朱悌") {   //御用饭勺
+        skill->ability.steam += 150; // 蒸技法+150
+        chef->tool = NO_TOOL;          // 不允许装备其他厨具
+    }
+
+    if (chef->name == "毛太郎") {    // 传世秘籍
+        skill->ability.add(40); // 全技法+40
+        chef->tool = NO_TOOL;          // 不允许装备其他厨具
+    }
+    
+    if (chef->name == "鼬名人") {    // 银骰子
+        chef->tool = NO_TOOL;          // 不允许装备其他厨具
+    }
+    
+    if (chef->name == "蜜桃桃") {    // 银骰子
+        chef->tool = NO_TOOL;          // 不允许装备其他厨具
+    }
+    
+    if (chef->name == "羽十六") {    // 传世秘籍
+        skill->ability.add(40); // 全技法+40
+        chef->tool = NO_TOOL;          // 不允许装备其他厨具
+    }
+    
+    if (chef->name == "珍艾") {    // 传世秘籍
+        skill->ability.add(40); // 全技法+40
+        chef->tool = NO_TOOL;          // 不允许装备其他厨具
+    }
 
     return; // 以下为示例代码，可自行修改。完成后注释此行。
 
-    ///////////// 不希望任何厨师装备厨具：
-
-    chef->tool = NO_TOOL;
-
-    ///////////// 自定义厨师装备的厨具：
-
-    auto skill = &chef->skill; // 这句不用管，放着就好。
-
-    if (chef->name == "达浪") {              // 金锄头
-        skill->ability.add(30);              // 全技法+30
-        skill->materialBuff.vegetable += 15; // 蔬菜售价+15%
-        chef->tool = NO_TOOL;                // 不允许装备其他厨具
-    }
-    if (chef->name == "宋书汐汐") {    // 幸福礼花炮
-        skill->ability.stirfry += 150; // 炒技法+150
-        skill->abilityBuff.bake += 25; // 烤售价+25%
-        chef->tool = NO_TOOL;          // 不允许装备其他厨具
-    }
 }
 #endif
