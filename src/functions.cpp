a8c5f0f282251e2d17c764227654232012c11132
5721006 <123737243+5721006@users.noreply.github.com>
2024-05-23 02:11:37 +0800

    更新 functions.cpp

--- src/functions.cpp
+++ src/functions.cpp
@@ -3,7 +3,6 @@
 #include "../config.hpp"
 #include "banquetRule.hpp"
 #include "exception.hpp"
-#include "activityRule.hpp"
 
 namespace r0 {
 States randomRecipe(States &, CList *, RList *, CRPairs *);
