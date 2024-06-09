342271d9d179620ffac0877e5d9b133def3089d8
5721006 <123737243+5721006@users.noreply.github.com>
2024-06-10 07:04:33 +0800

    更新 toolEquipped.hpp

--- toolEquipped.hpp
+++ toolEquipped.hpp
@@ -108,42 +108,6 @@ void toolEquipped(Chef *chef) {
         skill->materialBuff.vegetable += 15; // 菜售价+15%
         chef->tool = NO_TOOL;          // 不允许装备其他厨具
     }
-    if (chef->name == "尖椒鸡") {    // 反重力酒提
-        skill->ability.bake += 150; // 烤技法+150
-        chef->tool = NO_TOOL;          // 不允许装备其他厨具
-    }
-    if (chef->name == "朱悌") {   //御用饭勺
-        skill->ability.steam += 150; // 蒸技法+150
-        chef->tool = NO_TOOL;          // 不允许装备其他厨具
-    }
-    if (chef->name == "祖冲之") {   //御用饭勺
-        skill->ability.steam += 150; // 蒸技法+150
-        chef->tool = NO_TOOL;          // 不允许装备其他厨具
-    }
-    if (chef->name == "兰染") {    // 金猫筷子架
-        skill->ability.fry += 150; // 炸技法+150
-        chef->tool = NO_TOOL;          // 不允许装备其他厨具
-    }
-    if (chef->name == "蜜桃桃") {    // 传世秘籍
-        skill->ability.add(40);
-        chef->tool = NO_TOOL;          // 不允许装备其他厨具
-    }
-    if (chef->name == "毛太郎") {    // 传世秘籍
-        skill->ability.add(40);
-        chef->tool = NO_TOOL;          // 不允许装备其他厨具
-    }
-    if (chef->name == "鼬名人") {    // 传世秘籍
-        skill->ability.add(40);
-        chef->tool = NO_TOOL;          // 不允许装备其他厨具
-    }
-    if (chef->name == "苍胡颉") {    // 传世秘籍
-        skill->ability.add(40);
-        chef->tool = NO_TOOL;          // 不允许装备其他厨具
-    }
-    if (chef->name == "盐如玉") {    // 传世秘籍
-        skill->ability.add(40);
-        chef->tool = NO_TOOL;          // 不允许装备其他厨具
-    }
     return; // 以下为示例代码，可自行修改。完成后注释此行。
 }
 #endif
